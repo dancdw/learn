54b2289e56d76d0e17c2d4c61f4d93ad36ae5774
dancdw <dannychen11@163.com>
2018-02-22 15:35:05 +0800

    End Manage Output

--- JavaScript/webpack/src/index.js
+++ JavaScript/webpack/src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash' // 导入外部模块
-import './style.css' // 导入 css 文件
-import Img from './img.jpg' // 导入 image 文件
-import Package from './test.json' // 导入 json 文件
+import printMe from './print.js'
 
 
 function component() {
-  let element = document.createElement('div');
+  let element = document.createElement('div')
+  let btn = document.createElement('button')
 
   // 使用脚本
-  element.innerHTML = _.join(['Hello', 'webpack', JSON.stringify(Package)], ' ')
+  element.innerHTML = _.join(['Hello', 'webpack', '<br/>'], ' ')
+  btn.innerHTML = _.join(['Click me', 'and check the console!'])
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   return element;
 }
--- JavaScript/webpack/webpack.config.js
+++ JavaScript/webpack/webpack.config.js
@@ -1,10 +1,21 @@
 // import path from "path"
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), // 清除目录
+    new HtmlWebpackPlugin({ // 生成 html 文件
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js', // 根据入口名称生成输出文件名
     path: path.resolve(__dirname, 'dist')
   },
 }
