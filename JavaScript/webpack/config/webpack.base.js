9fbdb475031b47e3fceadcc33ae8a89fe6e4a54e
dancdw <dannychen11@163.com>
2018-02-23 22:39:14 +0800

    update Configuration

--- JavaScript/webpack/config/webpack.base.js
+++ JavaScript/webpack/config/webpack.base.js
@@ -1,50 +1,51 @@
 // import path from "path"
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-console.log(process.env.NODE_ENV)
-module.exports = {
-  context: __dirname,
-  entry: {
-    app: '../src/index.ts',
-    // print: './src/asset/print.js',
-    vendor: ['lodash'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src"),
-        use: 'ts-loader',
-        exclude: /node_module/,
-      },
-      // {
-      //   test: /\.css$/,
-      //   include: path.resolve(__dirname, "../src/asset"),
-      //   use: [
-      //     'style-loader',
-      //     'css-loader',
-      //   ],
-      // },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        include: path.resolve(__dirname, "../src/asset"),
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    
-  ],
-  output: {
-    filename: '[name].[chunkhash].js', // 根据入口名称生成输出文件名
-    path: path.resolve(__dirname, '../dist'), // 文件的输出路径
-    publicPath: '/', // 确保文件资源能正常访问
-  },
+module.exports = function(env) {
+  return {
+    context: __dirname,
+    entry: {
+      app: '../src/index.ts',
+      // print: './src/asset/print.js',
+      vendor: ['lodash'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: path.resolve(__dirname, "../src"),
+          use: 'ts-loader',
+          exclude: /node_module/,
+        },
+        {
+          test: /\.css$/,
+          include: path.resolve(__dirname, "../src/asset"),
+          use: (env && env.production) ? ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [ 'css-loader' ]
+          }) : [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          include: path.resolve(__dirname, "../src/asset"),
+          use: [
+            'file-loader',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: '[name].[chunkhash].js', // 根据入口名称生成输出文件名
+      path: path.resolve(__dirname, '../dist'), // 文件的输出路径
+      publicPath: '/', // 确保文件资源能正常访问
+    },
+  }
 }
--- JavaScript/webpack/webpack.prod.js
+++ JavaScript/webpack/webpack.prod.js
@@ -5,46 +5,32 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
 
-process.env.NODE_ENV = 'Production'
-// process.env.production = true
-
-module.exports = merge(base, {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, "../src/asset"),
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader' ]
-        }),
-      },
+module.exports = function(env) {
+  return merge(base(env), {
+    // devtool: 'source-map',
+    plugins: [
+      new ExtractTextPlugin({ // 提取 css 文件
+        filename: '[name].css'
+      }),
+      new webpack.DefinePlugin({ // 设置环境变量，在 src 目录里使用
+        'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV)
+      }),
+      new HtmlWebpackPlugin({ // 生成 html 文件
+        title: env.NODE_ENV,
+      }),
+      // new webpack.HashedModuleIdsPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({ // 将第三方模块提取成新的 Chunk
+        name: 'vendor', 
+      }),
+      new webpack.optimize.CommonsChunkPlugin({ // 将公共模块提取成新的 Chunk
+        name: 'manifest', 
+        minChunks: Infinity,
+      }),
+      new CleanWebpackPlugin(['./dist']), // 清除目录
+      new UglifyJSPlugin({ // tree shaking
+        // sourceMap: true,
+      }),
     ],
-  },
-  // devtool: 'source-map',
-  plugins: [
-    new ExtractTextPlugin({ // 提取 css 文件
-      filename: '[name].css'
-    }),
-    new webpack.DefinePlugin({ // 设置环境变量，在 src 目录里使用
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new HtmlWebpackPlugin({ // 生成 html 文件
-      title: process.env.NODE_ENV,
-    }),
-    // new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({ // 将第三方模块提取成新的 Chunk
-      name: 'vendor', 
-    }),
-    new webpack.optimize.CommonsChunkPlugin({ // 将公共模块提取成新的 Chunk
-      name: 'manifest', 
-      minChunks: Infinity,
-    }),
-    new CleanWebpackPlugin(['./dist']), // 清除目录
-    new UglifyJSPlugin({ // tree shaking
-      // sourceMap: true,
-    }),
-  ],
-})
+  })
+}
